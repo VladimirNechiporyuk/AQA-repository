518c190cc373b8f7347061ca2ca1e8665d0f8f94
VladimirNechiporyuk <magicfess@gmail.com>
2022-05-15 14:01:28 +0300

    Complete lesson 9

---
+++ lesson9/src/main/java/com/aqa/lessons/data/DataProvider.java
@@ -0,0 +1,65 @@
+package com.aqa.lessons.data;
+
+import java.util.Random;
+
+public class DataProvider {
+
+    private static DataProvider dataProvider;
+    private final Random random;
+
+    public static DataProvider getInstance() {
+        if (dataProvider == null) {
+            dataProvider = new DataProvider();
+        }
+        return dataProvider;
+    }
+
+    private DataProvider() {
+        random = new Random();
+    }
+
+    public String[] getHardcodeStringsArray() {
+        String[] strings = new String[11];
+        strings[0] = "мама";
+        strings[1] = "варова";
+        strings[2] = "папа";
+        strings[3] = "нана";
+        strings[4] = "шаша";
+        strings[5] = "тата";
+        strings[6] = "сагыса";
+        strings[7] = "зазиза";
+        strings[8] = "фафа";
+        strings[9] = "фофо";
+        strings[10] = "ел еж ежеле";
+        return strings;
+    }
+
+    public String[] getRandomWordsArray() {
+        int minArrayLength = 100;
+        int arrayLength = random.nextInt(900) + minArrayLength;
+        String[] strings = new String[arrayLength];
+        int arrayIndex = 0;
+        do {
+            strings[arrayIndex] = getWord();
+            arrayIndex++;
+        } while (arrayIndex < arrayLength);
+        return strings;
+    }
+
+    private String getWord() {
+        int wordLength = random.nextInt(5) + 1;
+        StringBuilder wordBuilder = new StringBuilder();
+        int wordIndex = 0;
+        while (wordIndex < wordLength) {
+            wordBuilder.append(getRandomAlphabetCharacter());
+            wordIndex++;
+        }
+        return wordBuilder.toString();
+    }
+
+    private char getRandomAlphabetCharacter() {
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        return alphabet[random.nextInt(alphabet.length)];
+    }
+
+}
